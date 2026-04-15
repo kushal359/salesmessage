f26234f82cee5eb320c722aabb7d8f67b8161ab7
kushal359 <kushal@teamenoch.com>
2026-04-15 14:14:12 -0500

    salesmessage updates

--- app/config/firebase.ts
+++ app/config/firebase.ts
@@ -1,10 +1,15 @@
-import admin from 'firebase-admin'
-import { ServiceAccount } from 'firebase-admin'
+import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || '{}');
+
   admin.initializeApp({
-    credential: admin.credential.cert(process.env.FIREBASE_SERVICE_ACCOUNT as ServiceAccount),
-  })
+    credential: admin.credential.cert({
+      ...serviceAccount,
+      // Fixes the most common 'Invalid PEM' error on Vercel
+      privateKey: serviceAccount.private_key?.replace(/\\n/g, '\n'),
+    }),
+  });
 }
 
-export const firebase = admin.firestore()
+export const firebase = admin.firestore();
